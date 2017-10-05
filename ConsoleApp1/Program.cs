c614c1513b663603648a1747b9c49324743bf170
fridaost2 <fridaost2@gmail.com>
2017-10-05 16:40:27 +0200

    min uppgift

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace
+namespace ConsoleApp1
+{
+    // Klass
+    class Program
+    {
+        // Metod
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In Main");
+            PrintStuff("test1");
+            PrintStuff("test2");
+            Console.WriteLine("Leaving Main");
+
+            Person.CelebrateBirthday();
+        }
+
+        static void PrintStuff(string valueToPrint)
+        {
+            Console.WriteLine("In PrintStuff");
+            Console.WriteLine(valueToPrint);
+            Console.WriteLine("Leaving PrintStuff");
+        
+                
+        }
+    }
+}
